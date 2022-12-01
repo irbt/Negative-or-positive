0535dee0756010a397e5215ff459acdf4baf1a25
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-12-01 14:51:49 +0100

    final

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
 ﻿//Given a number, write a method that returns number to the power of 2 if negative or square root if positive or zero.
-
+using System;
 namespace Program
 {
     class NegativeOrPositiveTASK
     {
-        public long NegativeOrPositive(double){
-            
-        }
-         static void Main(string[] args)
+        public static double NegativeOrPositive(double num)
         {
-            NegativeOrPositive(-2);// → 4
-            NegativeOrPositive(6.25);// → 2.5
+            return num<0 ? Math.Pow(num,2): Math.Sqrt(num);
+        }
+        public static void Main()
+        {
+            Console.WriteLine (NegativeOrPositive(-2));// → 4
+            Console.WriteLine (NegativeOrPositive(6.25));// → 2.5
 
         }
 
