e842282fd8ad66cdd57a37c67798b3f25d5de675
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-12-01 14:19:39 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Given a number, write a method that returns number to the power of 2 if negative or square root if positive or zero.
+
+namespace Program
+{
+    class NegativeOrPositiveTASK
+    {
+        public long NegativeOrPositive(double){
+            
+        }
+         static void Main(string[] args)
+        {
+            NegativeOrPositive(-2);// → 4
+            NegativeOrPositive(6.25);// → 2.5
+
+        }
+
+    }
+}
